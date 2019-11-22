0561588114fcc56ace1d2b88d9acd233cfc017ae
Anden Acitelli <a.acitelli@gmail.com>
2019-11-22 05:08:13 -0500

    Removed unnecessary content; Implemented basic routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Kanban from "./frontend/kanban";
+import Calendar from "./frontend/calendar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/c">
+            <Calendar />
+          </Route>
+          <Route path="/">
+            <Kanban />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
