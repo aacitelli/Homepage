850fe2b3ea5cd1ea5830a1d6042760f40e87af9f
Anden Acitelli <a.acitelli@gmail.com>
2020-01-02 22:54:14 -0500

    Did more frontend

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Kanban from "./frontend/kanban";
-import Calendar from "./frontend/calendar";
 
 export default function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/c">
-            <Calendar />
-          </Route>
           <Route path="/">
             <Kanban />
           </Route>
--- src/frontend/kanban.js
+++ src/frontend/kanban.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./kanban.css";
 
-export default function Kanban() {
-  return (
-    <div>Kanban!</div>
-  )
+export default class Kanban extends React.Component 
+{
+  render() {
+    return (
+      <div>
+
+        <div class="list">
+
+          <div class="listTitle">List 1</div>
+
+          <div class="card">
+            <div class="cardDescription">Card 1 Description</div>            
+            <div class="cardBottomContent">
+              <div class="cardDueDate">1/1/20</div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    )
+  }
 }
