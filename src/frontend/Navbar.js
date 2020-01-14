5bf0f29f1fe7a421320dfa848fde3262964b3d5d
Anden Acitelli <a.acitelli@gmail.com>
2020-01-14 18:00:23 -0500

    Fix button linking issue

--- src/frontend/Navbar.js
+++ src/frontend/Navbar.js
@@ -5,11 +5,13 @@ import "./navbar.css";
 export default function Navbar(props) {
 
     let buttonText = props.id === "kanban" ? "Calendar" : "Kanban"; 
+    let buttonLink = props.id === "kanban" ? "/c" : "/k"; 
+
     return (
         <nav>
             <div class="navbarLeftContent"></div>
             <div class="navbarRightContent">
-                <Link to="/c">
+                <Link to={buttonLink}>
                     <div id="calendarButtonContainer">
                         <p>{buttonText}</p>
                     </div> 
