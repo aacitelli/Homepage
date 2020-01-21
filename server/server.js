e08cca1c190a21694b72bbb7eabd6dadb8d15aa8
Anden Acitelli <a.acitelli@gmail.com>
2020-01-20 20:14:10 -0500

    Finished database connection implementaiton

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
-const express = require("express"); 
+// npm imports
+const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const app = express(); 
-app.use(bodyParser.urlencoded({ extended: false })) 
+// Set up express 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set up database connection w/ error handlers
+require("dotenv").config({ path: "server/.env" });
+mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@homepage-nwxpq.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB Connection Error: "));
+db.on("connected", console.log.bind(console, "Successfully Connected to MongoDB Database!"));
+
+// Physically start up backend server 
 app.listen(3001, () => {
     console.log("Express server is running on localhost:3001");
-})
+});
