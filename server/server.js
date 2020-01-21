2b72688e902ac874bcac01e513e17f32c7fb8803
Anden Acitelli <a.acitelli@gmail.com>
2020-01-20 21:17:44 -0500

    Finished frontend baseplate HTTP requests

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // Set up database connection w/ error handlers
 require("dotenv").config({ path: "server/.env" });
-mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@homepage-nwxpq.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@homepage-nwxpq.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB Connection Error: "));
 db.on("connected", console.log.bind(console, "Successfully Connected to MongoDB Database!"));
@@ -21,6 +21,16 @@ app.post("/api/kanban/board", function(req, res) {
      
 });
 
+// Add List to Board 
+app.post("/api/kanban/list", function(req, res) {
+
+});
+
+// Add Task to List 
+app.post("/api/kanban/task", function(req, res) {
+
+});
+
 //! READ Operations
 // Read Board 
 app.get("/api/kanban/board", function(req, res) {
@@ -28,29 +38,20 @@ app.get("/api/kanban/board", function(req, res) {
 });
 
 //! UPDATE operations 
-// Add List to Board 
-app.put("/api/kanban/list", function(req, res) {
-
-});
-
-// Add Task to List 
-app.put("/api/kanban/task", function(req, res) {
-
-});
-
-// Delete List from Board 
-app.put("/api/kanban/list", function(req, res) {
-
-});
-
-// Delete Task from List 
-app.put("/api/kanban/task", function(req, res) {
-
-});
 
 //! DELETE operations
 // Delete Board 
 app.delete("/api/kanban/board", function(req, res) {
+
+});
+
+// Delete List from Board 
+app.delete("/api/kanban/list", function(req, res) {
+
+});
+
+// Delete Task from List 
+app.delete("/api/kanban/task", function(req, res) {
 
 });
 
--- src/frontend/kanban.js
+++ src/frontend/kanban.js
@@ -10,16 +10,16 @@ export default class Kanban extends React.Component
       <div>
         <Navbar id="kanban" />     
         <div id="wrapper">
-          <button onClick={exampleRequest}>Click Me!</button>
-          <div className="list">
-            <div className="listTitle">List 1</div>
-            <div className="card">
-              <div className="cardDescription">Card 1 Description</div>            
-              <div className="cardBottomContent">
-                <div className="cardDueDate">1/1/20</div>
-              </div>
-            </div>
-          </div>
+
+          <button onClick={readBoard}>Get Board Contents</button>
+          <button onClick={addBoard}>Add Board</button>      
+          <button onClick={deleteBoard}>Delete Board</button>
+
+          <button onClick={addListToBoard}>Add List To Board</button>
+          <button onClick={deleteListFromBoard}>Remove List From Board</button>
+
+          <button onClick={addTaskToList}>Add Task To List</button>
+          <button onClick={deleteTaskFromList}>Remove Task From List</button>
 
           <div className="list">
             <div className="listTitle">List 1</div>
@@ -30,29 +30,24 @@ export default class Kanban extends React.Component
               </div>
             </div>
           </div>
-
-          <div className="list">
-            <div className="listTitle">List 1</div>
-            <div className="card">
-              <div className="cardDescription">Card 1 Description</div>            
-              <div className="cardBottomContent">
-                <div className="cardDueDate">1/1/20</div>
-              </div>
-            </div>
-          </div>
+          
         </div>
-      </div>
-      
+        
+      </div>      
     )
   }
 }
 
-async function exampleRequest() {
-  const data = {
-    firstName: "Anden", 
-    lastName: "Acitelli"
-  };
+// Get board data from the server 
+// TODO: Finish implementation 
+async function readBoard() {
+  const response = await fetch("/api/kanban/board"); 
+}
 
+// Add an entirely new board to the database 
+// TODO: Finish implementation
+async function addBoard() {
+  let data = {};
   const response = await fetch("/api/kanban/board", {
     method: "POST", 
     mode: "cors", 
@@ -62,6 +57,79 @@ async function exampleRequest() {
     }, 
     body: JSON.stringify(data)
   });
+}
 
-  console.log("Server Response: " + response);
+// Remove a board from the database 
+// TODO: Finish implementation
+async function deleteBoard() {
+  let data = {};
+  const response = await fetch("/api/kanban/board", {
+    method: "DELETE", 
+    mode: "cors", 
+    credentials: "same-origin", 
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(data)
+  });
+}
+
+// Add a list to the current board 
+// TODO: Finish implementation 
+async function addListToBoard() {
+  let data = {};
+  const response = await fetch("/api/kanban/list", {
+    method: "POST", 
+    mode: "cors", 
+    credentials: "same-origin", 
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(data)
+  });
+}
+
+// Remove a list from the current board 
+// TODO: Finish implementation
+async function deleteListFromBoard() {
+  let data = {};
+  const response = await fetch("/api/kanban/list", {
+    method: "DELETE", 
+    mode: "cors", 
+    credentials: "same-origin", 
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(data)
+  });
+}
+
+// Add a task to a list 
+// TODO: Finish implementation
+async function addTaskToList() {
+  let data = {};
+  const response = await fetch("/api/kanban/task", {
+    method: "POST", 
+    mode: "cors", 
+    credentials: "same-origin", 
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(data)
+  });
+}
+
+// Remove a task from a list 
+// TODO: Finish implementation
+async function deleteTaskFromList() {
+  let data = {};
+  const response = await fetch("/api/kanban/task", {
+    method: "DELETE", 
+    mode: "cors", 
+    credentials: "same-origin", 
+    headers: {
+      "Content-Type": "application/json"
+    }, 
+    body: JSON.stringify(data)
+  });
 }
