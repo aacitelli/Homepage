e52730bbe657948638b87134cb513b58534e0b7f
Anden Acitelli <a.acitelli@gmail.com>
2020-01-20 20:36:33 -0500

    misc. changes

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB Connection Error: "));
 db.on("connected", console.log.bind(console, "Successfully Connected to MongoDB Database!"));
 
+//! CREATE operations
+// Add list 
+// Add task to list
+
+//! READ Operations
+
+//! UPDATE operations 
+// Change text of task 
+
+//! DELETE operations
+// Delete list 
+// Delete task from list 
+
 // Physically start up backend server 
 app.listen(3001, () => {
     console.log("Express server is running on localhost:3001");
